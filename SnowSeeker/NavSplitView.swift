5f7f78ede6acc4b3ed578fc7dc69bf1cb57884f5
Marcus Benoit <benoit.marcus@gmail.com>
2024-09-23 14:23:19 +0200

    Working with two side by side windows

---
+++ SnowSeeker/NavSplitView.swift
@@ -0,0 +1,18 @@
+//
+//  NavSplitView.swift
+//  SnowSeeker
+//
+//  Created by Marcus Benoit on 15.09.24.
+//
+
+import SwiftUI
+
+struct NavSplitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavSplitView()
+}
